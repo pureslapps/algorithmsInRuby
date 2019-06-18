b8af1bff787bffd2741c81f26b93430a4e7db9f9
Jorge Bhagwandeen <jorgebhagwandeen@yahoo.com>
2019-06-18 15:06:35 -0400

    completed the god damn array problem

--- arrays.rb
+++ arrays.rb
@@ -1,12 +1,12 @@
 nums = Array[11, 4, 5, 12, 9, 1, 8, 5] # created new array
 
 def array_split(list)
-    list2 = list[3..length]
-    newList = list.sort
+    list2 = list[3..list.length]
+    newList = list[0..2].sort
     newList2 = list2.sort
     puts newList
     puts newList2
-     megaList = newList.join(newList2)
+     megaList = [newList, newList2].join
      puts megaList
    end
 
